f6b85f801f65a12baa5202e56c51d2c691c6be58
marselomorales <163350569+marselomorales@users.noreply.github.com>
2024-04-24 20:36:09 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -39,9 +39,16 @@ int is_equal(void* key1, void* key2){
 }
 
 
-void insertMap(HashMap * map, char * key, void * value) {
+void insertMap(HashMap * map, char * key, void * value) 
+{
+  if(map==NULL || key==NULL) return;
 
+  long posicion = hash(key,map->capacity);
 
+  while(map->buckets[posicion]!=NULL && map->buckets[posicion]->key != NULL)
+    {
+      if(is_equal(map->buckets[posicion]->key,key)) 
+    }
 }
 
 void enlarge(HashMap * map) {
