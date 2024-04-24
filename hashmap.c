be2a5b4fe457afbbc79d311688bd6237bddfbd07
marselomorales <163350569+marselomorales@users.noreply.github.com>
2024-04-24 20:57:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,14 +80,16 @@ void eraseMap(HashMap * map,  char * key) {
 
 }
 
-Pair * searchMap(HashMap * map,  char * key) {   
-    if(map == NULL || key == NULL) 
-        return NULL;
+Pair * searchMap(HashMap * map,  char * key) 
+{   
+    if(map == NULL || key == NULL) return NULL;
 
     long posicion = hash(key, map->capacity);
 
-    while(map->buckets[posicion] != NULL) {
-        if(map->buckets[posicion]->key != NULL && is_equal(map->buckets[posicion]->key, key)) {
+    while(map->buckets[posicion] != NULL) 
+    {
+        if(map->buckets[posicion]->key != NULL && is_equal(map->buckets[posicion]->key, key)) 
+        {
             map->current = posicion;
             return map->buckets[posicion];
         }
